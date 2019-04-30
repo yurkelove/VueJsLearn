a6d82273d0b23ba6b7d96be80b50a34323672581
Yura Kuzkin <yurkelove28@gmail.com>
2019-05-01 00:27:46 +0300

    events,data-binding,li-list-dynamic

--- vue.js
+++ vue.js
@@ -3,11 +3,39 @@ new Vue({
     el: "#app",
     data: {
         name: 'Yura',
-        job: 'Im front end developer'
+        job: 'Im front end developer',
+        //Привязка данных(data-binding)
+        link: 'http://google.com',
+        markup: '<a href="http://google.com">Link</a>',
+        //events
+        counter: 0,
+        //Динамический лист вставить
+        people:[
+            {
+                nameP: 'Frank',
+                stillAlive:false,
+            },
+            {
+                nameP: 'Alex',
+                stillAlive:true,
+            },
+            {
+                nameP: 'John',
+                stillAlive:false,
+            }
+        ],
+     
     },
+    // methods
     methods: {
         greet: function(){
             return 'Hello me name is ' + this.name;
+        },
+        //event-обработчик через функцию
+        substract: function(){
+            this.counter--;
         }
     }
+    
+
 })
